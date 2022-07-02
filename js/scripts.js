6a83dcf1ad13f3ab490b052aad5ee77e2b1f255b
Rioran <voronov_rv@mail.ru>
2022-07-03 01:24:36 +0300

    + start button

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 var ms_render_delay = 40;
 var time_cache = new Date().toTimeString().slice(0,8);
-var clock_digits_p = document.getElementById("clock-digits-p");
+// var clock_digits_p = document.getElementById("clock-digits-p");
 
 function render_time() {
     var time = new Date().toTimeString().slice(0,8);
     if (time !== time_cache) {
-      clock_digits_p.innerHTML = time;
-      time_cache = time;
+        clock_digits_p.innerHTML = time;
+        time_cache = time;
     }
 }
 
 function render_loop() {
-  setInterval(function(){
-    render_time();
-  }, ms_render_delay);
+    setInterval(function(){
+        render_time();
+    }, ms_render_delay);
 }
 
-render_loop();
+function remove_by_id(target_id) {
+    var target_element = document.getElementById(target_id);
+    target_element.remove()
+}
+
+// render_loop();
